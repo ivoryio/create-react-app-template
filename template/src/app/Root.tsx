9fa1092a2852f4efb288a158e16fd40cf051e1b7
Lucian Vasiliu <lucian@thinslices.com>
2020-05-12 18:08:44 +0300

    Improve folder structure

--- template/src/app/Root.tsx
+++ template/src/app/Root.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from 'styled-components'
 import './assets/global.css'
 
 import { theme } from './assets/theme'
-import { AppRouter } from './navigation/Router'
+import { AppRouter } from './screens/Router'
 
 export const Root = () => (
   <ThemeProvider theme={theme}>
--- template/src/app/navigation/Router.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Router } from '@reach/router'
-import { Info } from 'app/screens/Info'
-
-export const AppRouter = () => (
-  <Router>
-    <Info default />
-  </Router>
-)
