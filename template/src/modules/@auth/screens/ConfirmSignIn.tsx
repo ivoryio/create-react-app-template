aca72a710d8189e3c7e347972c08a183b927566a
ts.mihail <mihail.salari@thinslices.com>
2020-06-23 16:04:59 +0300

    Add DynamicForm and Replace all forms with it

--- template/src/modules/@auth/screens/ConfirmSignIn.tsx
+++ template/src/modules/@auth/screens/ConfirmSignIn.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react'
-import { Grid, Container } from '@material-ui/core'
+import { Grid } from '@material-ui/core'
 import { useForm } from 'react-hook-form'
 
 import { t, i18nKeys } from 'locales/i18n'
 import { useToast } from 'hooks/useToast'
 
-import { Button, Input } from 'app/components'
+import { DynamicForm } from 'app/components'
 import { useConfirmSignIn } from '../hooks'
+import { confirmSignInLayout, confirmSignInActions } from '../constants'
 import { FormHeader, FormFooter, ChangeAuthStateLink } from '../components'
 
 const authKeys = i18nKeys.auth
@@ -36,34 +37,38 @@ export const ConfirmSignIn: React.FC = () => {
   }
 
   return (
-    <form data-testid='confirmSignInForm' onSubmit={handleSubmit(onSubmit)}>
-      <Container maxWidth='xs'>
-        <FormHeader data-testid='confirm-sign-in-form-header'>
-          {t(authKeys.confirmSignIn.header)}
-        </FormHeader>
-        <Input
-          dataTestId='confirm-sign-in-code-input'
-          rules={{ required: true }}
-          name='code'
-          label={t(authKeys.labels.confirmationCode)}
-          autoComplete='code'
-          control={control}
-        />
-        <FormFooter>
-          <Button data-testid='confirm-sign-in-btn' type='submit' fullWidth>
-            {t(authKeys.confirmSignIn.actions.confirm)}
-          </Button>
-          <Grid container>
-            <Grid item>
-              <ChangeAuthStateLink
-                label={t(authKeys.confirmSignIn.actions.backToSignIn)}
-                newState='signIn'
-              />
-            </Grid>
-          </Grid>
-        </FormFooter>
-      </Container>
+    <>
+      <DynamicForm
+        actions={confirmSignInActions({ confirm: t(authKeys.confirmSignIn.actions.confirm) })}
+        control={control}
+        dataTestId='confirmSignInForm'
+        handleSubmit={handleSubmit}
+        layout={confirmSignInLayout({
+          confirmationCode: t(authKeys.labels.confirmationCode),
+        })}
+        name='confirm-sign-in'
+        onSubmit={onSubmit}
+        FormHeader={() => (
+          <FormHeader data-testid='confirm-sign-in-form-header'>
+            {t(authKeys.confirmSignIn.header)}
+          </FormHeader>
+        )}
+        FormFooter={() => <ConfirmSignInFormFooter />}
+      />
       <Toast />
-    </form>
+    </>
   )
 }
+
+const ConfirmSignInFormFooter = () => (
+  <FormFooter>
+    <Grid container>
+      <Grid item>
+        <ChangeAuthStateLink
+          label={t(authKeys.confirmSignIn.actions.backToSignIn)}
+          newState='signIn'
+        />
+      </Grid>
+    </Grid>
+  </FormFooter>
+)
